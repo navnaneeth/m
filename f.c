ac27829b4930605e1744ee9c1167a49b4edbedf1
navaneeth <navaneethk178@gmail.com>
2024-07-19 11:52:10 +0530

    se commit

---
+++ f.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+int main(){
+
+    
+
+    int a=8;
+    int b=9;
+    //int c=a+b
+    printf("%d",a+b);
+    printf("\n%d",a*b);
+    printf("\n%d",a-b);
+    printf("\n%d",a%b);
+    printf("\n%d",a/b);
+
+ return 0;
+}
---
+++ ff.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+int c=6;
+int d=9;
+int f=c+d;
+
+printf("%d",f);
+
+return 0;
+
+}
+
+
+
